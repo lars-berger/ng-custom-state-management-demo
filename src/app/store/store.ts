720285fa3edeb08e7b0e591e376265e65f26e4a9
Lars Berger <lars.berger@yahoo.com>
2022-02-04 23:54:10 +0100

    feat: emit state changes to redux devtools

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -21,11 +21,21 @@ export abstract class Store<T> {
     return this.storeConfig.initializerFn;
   }
 
+  private reduxDevToolRef =
+    (window as any)['__REDUX_DEVTOOLS_EXTENSION__'] ||
+    (window as any)['devToolsExtension'];
+
+  private reduxDevToolConnection = this.reduxDevToolRef.connect({
+    name: 'Test123',
+    instanceId: 'Test123',
+  });
+
   /** Observable of changes to state. Includes previous and incoming value. */
   changes$: Observable<StoreChange<T>> = new Subject();
 
   constructor(private storeConfig: StoreConfig<T>) {
     this.state = new BehaviorSubject(this.getFreshState());
+    this.emitViaDevtool('@@INIT', this.getFreshState());
   }
 
   get(): Observable<T> {
@@ -40,15 +50,18 @@ export abstract class Store<T> {
     return this.get().pipe(map(predicate), distinctUntilChanged());
   }
 
-  patch(args: Partial<T>): void {
-    return this.state.next({
+  patch(partialState: Partial<T>): void {
+    const updatedState: T = {
       ...this.state.value,
-      args,
-    });
+      ...partialState,
+    };
+    this.state.next(updatedState);
+    this.emitViaDevtool('Patch', updatedState);
   }
 
-  set(args: T): void {
-    return this.state.next(args);
+  set(state: T): void {
+    this.state.next(state);
+    this.emitViaDevtool('Set', state);
   }
 
   reset(): void {
@@ -65,5 +78,15 @@ export abstract class Store<T> {
 
   private getFreshState(): T {
     return this.initializerFn();
+  }
+
+  /** Emit updated state to Redux devtool. */
+  private emitViaDevtool(actionName: string, state: T): void {
+    this.reduxDevToolConnection.send(
+      `[${this.name}] ${actionName}`,
+      { [this.name]: state },
+      false,
+      'Test123'
+    );
   }
 }
