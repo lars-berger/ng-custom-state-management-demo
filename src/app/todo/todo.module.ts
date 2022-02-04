d9aa3b34ed456b9746f4ad9aa0bbaa87c0473a7b
Lars Berger <lars.berger@yahoo.com>
2022-02-04 19:38:04 +0100

    feat: generate `TodoListComponent`

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TodoListComponent } from './todo-list/todo-list.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [TodoListComponent],
   imports: [CommonModule],
+  exports: [TodoListComponent],
 })
 export class TodoModule {}
